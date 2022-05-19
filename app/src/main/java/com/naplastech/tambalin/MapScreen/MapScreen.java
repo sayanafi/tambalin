a0eb5f12a9cb4b355472c3fcc277102552518822
zulkarnainspj <zulemail.sepanjang@gmail.com>
2022-05-19 21:10:07 +0700

    OSM Library

--- app/src/main/java/com/naplastech/tambalin/MapScreen/MapScreen.java
+++ app/src/main/java/com/naplastech/tambalin/MapScreen/MapScreen.java
@@ -1,15 +1,16 @@
-package com.naplastech.tambalin.MainScreen;
+package com.naplastech.tambalin.MapScreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 import com.naplastech.tambalin.R;
 
-public class HomeP extends AppCompatActivity {
+public class MapScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_p);
+        setContentView(R.layout.activity_map_screen);
     }
 }
