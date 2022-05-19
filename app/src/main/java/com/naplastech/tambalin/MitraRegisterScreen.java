16f5d6e5f3a8706deb24b41adec0ba021f14bdc9
GummyBearz <75606963+Syahfril@users.noreply.github.com>
2022-05-20 03:48:21 +0700

    #2ggh53b

---
+++ app/src/main/java/com/naplastech/tambalin/MitraRegisterScreen.java
@@ -0,0 +1,14 @@
+package com.naplastech.tambalin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MitraRegisterScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mitra_register);
+    }
+}
