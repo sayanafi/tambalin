d74337c008e1ed095d9da4f66cc205ad8abe0f63
Muhammad Nafi Udin <sayanafime@gmail.com>
2022-05-19 07:46:21 +0700

    update core

--- app/src/main/java/com/naplastech/tambalin/OnboardingScreen.java
+++ app/src/main/java/com/naplastech/tambalin/OnboardingScreen.java
@@ -1,4 +1,14 @@
 package com.naplastech.tambalin;
 
-public class OnboardingScreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnboardingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.onboarding_screen);
+    }
 }
