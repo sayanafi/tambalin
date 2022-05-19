c0b20c7469eb7bcd68a2e3348de2bc8ba04a5967
Muhammad Nafi Udin <sayanafime@gmail.com>
2022-05-19 12:55:04 +0700

    new screen

---
+++ app/src/main/java/com/naplastech/tambalin/MainScreen/HomeM.java
@@ -0,0 +1,14 @@
+package com.naplastech.tambalin.MainScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_m);
+    }
+}
