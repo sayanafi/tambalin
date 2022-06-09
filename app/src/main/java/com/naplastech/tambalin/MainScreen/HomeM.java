e1ba464ffe7a2d2990415fc045161f8f863a7f8b
Muhammad Nafi Udin <sayanafime@gmail.com>
2022-06-09 13:25:54 +0700

    update mitra #2hrhp2y

--- app/src/main/java/com/naplastech/tambalin/MainScreen/HomeM.java
+++ app/src/main/java/com/naplastech/tambalin/MainScreen/HomeM.java
@@ -11,6 +11,7 @@ public class HomeM extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_home_m);
     }
 }
