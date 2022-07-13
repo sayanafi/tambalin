46223baf23e009d1490eecad2c0c490e7c391c6d
Muhammad Nafi Udin <sayanafime@gmail.com>
2022-07-13 14:24:51 +0700

    fragment

--- app/src/main/java/com/naplastech/tambalin/MainScreen/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/naplastech/tambalin/MainScreen/ui/dashboard/DashboardFragment.java
@@ -1,28 +1,16 @@
 package com.naplastech.tambalin.MainScreen.ui.dashboard;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.naplastech.tambalin.MainScreen.R;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+    /*public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View root = inflater.inflate(com.naplastech.tambalin.R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -30,6 +18,5 @@ public class DashboardFragment extends Fragment {
                 textView.setText(s);
             }
         });
-        return root;
+        return root;*/
     }
-}
--- app/src/main/java/com/naplastech/tambalin/MainScreen/ui/home/HomeFragment.java
+++ app/src/main/java/com/naplastech/tambalin/MainScreen/ui/home/HomeFragment.java
@@ -1,16 +1,6 @@
 package com.naplastech.tambalin.MainScreen.ui.home;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+/*import androidx.lifecycle.ViewModelProvider;
 
 import com.naplastech.tambalin.MainScreen.R;
 
@@ -31,5 +21,4 @@ public class HomeFragment extends Fragment {
             }
         });
         return root;
-    }
-}
+    }*/
